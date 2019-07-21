77123672237bb0ade5535e418b7691de676eccf5
chikuwa111 <take0shun1111@gmail.com>
2019-07-22 03:18:57 +0900

    Add validateRequest func

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ function doPost(
   e: GoogleAppsScript.Events.DoPost
 ): GoogleAppsScript.Content.TextOutput {
   const param = e.parameter as Parameter;
+  const [isValid, errorMessage] = validateRequest(param);
+  if (!isValid) {
+    const response = { text: errorMessage };
+    return ContentService.createTextOutput(
+      JSON.stringify(response)
+    ).setMimeType(ContentService.MimeType.JSON);
+  }
 
-  // ここでリクエストが自分のslackからのものかチェックしたいが、
-  // tokenはdeprecatedかつheaderもdoPostでは取れないので厳しい
-
-  const [power, time, type] = param.text.split(' ');
+  const [power, time, type] = param.text.split(' ').map(s => s.toLowerCase());
 
   const response = {
     text: `power: ${power}, time: ${time}, type: ${type}`,
@@ -19,3 +23,13 @@ function doPost(
     ContentService.MimeType.JSON
   );
 }
+
+const validateRequest = (param: Parameter): [boolean, string] => {
+  // ここでリクエストが自分のslackからのものかチェックしたいが、
+  // tokenはdeprecatedかつheaderもdoPostでは取れないので厳しい
+  const power = param.text.split(' ')[0].toLowerCase();
+  if (!['on', 'off'].includes(power)) {
+    return [false, 'Invalid parameter'];
+  }
+  return [true, ''];
+};
