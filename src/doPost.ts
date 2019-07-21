f90e800e75d049e5e69a7e89bd903bbb793eea8e
chikuwa111 <take0shun1111@gmail.com>
2019-07-22 02:43:54 +0900

    Add test code

---
+++ src/doPost.ts
@@ -0,0 +1,21 @@
+type Parameter = {
+  text: string;
+};
+
+function doPost(
+  e: GoogleAppsScript.Events.DoPost
+): GoogleAppsScript.Content.TextOutput {
+  const param = e.parameter as Parameter;
+
+  // ここでリクエストが自分のslackからのものかチェックしたいが、
+  // tokenはdeprecatedかつheaderもdoPostでは取れないので厳しい
+
+  const [_, power, time, type] = param.text.split(' ');
+
+  const response = {
+    text: `power: ${power}, time: ${time}, type: ${type}`,
+  };
+  return ContentService.createTextOutput(JSON.stringify(response)).setMimeType(
+    ContentService.MimeType.JSON
+  );
+}
